367b561dbe3f3f85cb7ad6abe0f762793b7cee0f
Caio Ferreira Silva <caio@ferreirasilva.com.br>
2017-08-03 10:11:38 -0300

    Improving extension's console outputs

--- chrome-extension/debugger.js
+++ chrome-extension/debugger.js
@@ -8,13 +8,12 @@ function parseHeaders(header) {
 	if (!header.name.match(/^Debugger\|/)) {
 		return;
 	}
-	var headerInfo = header.name.split('|')
-	output(headerInfo[0], headerInfo[1], JSON.parse(header.value))
+	output(JSON.parse(header.value));
 }
 
-function output(name, level, message) {
-	var date = new Date;
-	var output = '[' + name + ' ';
+function output(message) {
+	var date = new Date(message.time * 1000);
+	var output = '[';
 	output += date.getFullYear() + '-';
 	output += ('00' + date.getMonth()).substr(-2) + '-';
 	output += ('00' + date.getDay()).substr(-2) + ' ';
@@ -22,5 +21,5 @@ function output(name, level, message) {
 	output += ('00' + date.getMinutes()).substr(-2) + ':';
 	output += ('00' + date.getSeconds()).substr(-2) + '.';
 	output += date.getMilliseconds() + ']';
-	console[level](output, message);
+	console[message.level](output, '[' + message.label + ']', message.information);
 }
