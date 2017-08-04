ed8bab74b99c216e35fe9c605453af6fa5fe9136
Caio Ferreira Silva <caio@ferreirasilva.com.br>
2017-08-04 18:27:59 -0300

    Changing user-agent within extension

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,9 +1,21 @@
 chrome.webRequest.onHeadersReceived.addListener(function(info) {
-	chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+	chrome.tabs.onUpdated.addListener(function (tabId, changeInfo) {
 		if (!info || tabId !== info.tabId || changeInfo.status !== 'complete') {
 			return;
 		}
 		chrome.tabs.sendMessage(info.tabId, info.responseHeaders);
 		info = null;
 	});
-}, {urls: ["*://*/*"]}, ["responseHeaders"]);
+}, {urls: ['<all_urls>']}, ['responseHeaders']);
+
+chrome.webRequest.onBeforeSendHeaders.addListener(function(info) {
+	var headers = info.requestHeaders, response = {};
+	for (var i = 0; i < headers.length; ++i) {
+		if (headers[i].name === 'User-Agent') {
+			headers[i].value = headers[i].value + ' cfs100/debugger';
+			break;
+		}
+	}
+	response.requestHeaders = headers;
+	return response;
+}, {urls: ['<all_urls>']}, ['requestHeaders', 'blocking']);
